3e8d1e5f412d73c76fdd8eeaf03f9924fced5679
LeamCohen <137171605+LeamCohen@users.noreply.github.com>
2024-07-05 15:57:28 +0300

    Update UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
-GIT_REPO_URL="hhttps://github.com/LeamCohen/Portf.git"
+GIT_REPO_URL="https://github.com/LeamCohen/Portf.git"
 
 # If using Private Repo
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
